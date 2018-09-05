2847a10e7de8567e09bf522e3b45ad8a939ac820
Anders Latif <anders@github.com>
2018-09-05 13:40:33 +0200

    my first API

--- 2._Our_First_API/app.js
+++ 2._Our_First_API/app.js
@@ -1,4 +1,23 @@
 var express = require("express");
 var app = express();
 
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+// callback function
+app.get("/path", function(req, res) {
+    res.send("responsey pony");
+});
+
+app.get("/who_are_you", function(req, res) {
+    var me = {"name": "Anders"};
+    res.send(me);
+});
+
+app.get("/sendStatus", function(req, res) {
+    res.sendStatus(404);
+});
+
 app.listen(3000);
