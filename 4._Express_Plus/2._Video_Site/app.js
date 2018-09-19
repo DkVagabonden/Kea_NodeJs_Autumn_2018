e35906e836db5b457909de3c3f5822c2b4fa32d8
Anders Latif <anders@github.com>
2018-09-19 13:19:22 +0200

    Video works

--- 4._Express_Plus/2._Video_Site/app.js
+++ 4._Express_Plus/2._Video_Site/app.js
@@ -5,7 +5,12 @@ app.use(express.static('public'));
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/public/home.html");
-})
+});
+
+// todo add req param id
+app.get("/player", function(req, res) {
+    res.sendFile(__dirname + "/public/player.html");
+});
 
 
 const server = app.listen("3000", function(err) {
