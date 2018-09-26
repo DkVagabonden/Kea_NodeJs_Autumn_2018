d3c083cb95f618f4bf7ce1ac6f5e070cbd5e7704
Anders Latif <anders@github.com>
2018-09-26 14:57:25 +0200

    Header and footer injecting

---
+++ 5._Ajax/2._Video_Site/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static('public'));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/public/home.html");
+});
+
+// todo get video id from query string
+app.get("/player", function(req, res) {
+    res.sendFile(__dirname + "/public/player.html");
+});
+
+
+const server = app.listen("3000", function(err) {
+    if (err) {
+        console.log("Error running server on port ", server.address().port);
+    } else {
+        console.log("Server running on port", server.address().port);
+    }
+});
