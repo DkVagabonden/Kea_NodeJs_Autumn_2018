a622a971db9297ccfe8a7c57373b05c85cf6dac5
Anders Latif <anders@github.com>
2018-09-26 11:20:26 +0200

    included bodyparser and learning about ajax

--- 4._Express_Plus/2._Video_Site/public/main.js
+++ 4._Express_Plus/2._Video_Site/public/main.js
@@ -1,4 +1,15 @@
 let script = document.createElement('script');
 script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js';
 script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
+
+script.onreadystatechange = handler;
+script.onload = handler;
+
+var head = document.getElementsByTagName('head')[0];
+// Fire the loading
+head.appendChild(script);
+
+function handler() {
+    $("#header").load("/components/header.html");
+    $("#footer").load("/components/footer.html");
+}
---
+++ 5._Ajax/1._Ajax/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/submit-form", function(req, res) {
+    console.log("body", req.body);
+    res.send("form submitted");
+});
+
+// Create an endpoint where the first part of the url is /get-info
+// it should take the first name and last name dynamically
+// and it should take a value called geo from query string
+// put it in a JSON and return to the browser as JSON
+
+// URL example: localhost:3000/get-info/Dennis/Petersen?geo=Danmark
+
+app.get("/get-info/:firstname/:surname", function(req, res) {
+    const person = {
+        "geo": req.query.geo,
+        "firstname": req.params.firstname,
+        "surname": req.params.surname
+    }
+
+    res.json(person);
+});
+
+
+app.listen("3000", function(err) {
+    if (err) {
+        console.log("Error running the server", 3000)
+    } else {
+        console.log("Server running on port 3000");
+    }
+});
