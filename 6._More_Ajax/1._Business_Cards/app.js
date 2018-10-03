0d5ad82950421016f2b545aece68d672a944ab56
Anders Latif <anders@github.com>
2018-10-03 09:32:42 +0200

    created the business cards exercise

---
+++ 6._More_Ajax/1._Business_Cards/app.js
@@ -0,0 +1,15 @@
+
+/*1. 
+Customer: I want a website with a form where you can submit business cards
+Developer colleague: Create to endpoints in the backend called /get-business-cards and /add-business-card
+*/
+
+let businessCards = [];
+
+let initialCompany = {
+    "name": "Client",
+    "position": "IT"
+};
+
+businessCards.push(initialCompany);
+
