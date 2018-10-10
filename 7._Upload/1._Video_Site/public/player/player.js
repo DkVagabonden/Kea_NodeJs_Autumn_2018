d1318f72310a6a4a20f47fdfd52f825ca0f5eefa
Anders Latif <anders@github.com>
2018-10-10 13:06:27 +0200

    styled the footer

--- 7._Upload/1._Video_Site/public/player/player.js
+++ 7._Upload/1._Video_Site/public/player/player.js
@@ -1,6 +1,7 @@
 let urlParams = new URLSearchParams(window.location.search);
 let videoId = urlParams.get('video');
 
-const player = "<video width='640' height='480' src='/videos/" + videoId + "' controls>Sorry, your browser doesn't support embedded videos.</video>"
+const player = "<video width='1024' height='768' src='/videos/" + videoId + "' controls>" +
+    "Sorry, your browser doesn't support embedded videos.</video>"
 
 $("#player").append(player);
