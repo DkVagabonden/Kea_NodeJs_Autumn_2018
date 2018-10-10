872910064bddb7583eb3d94152697d427d644bbf
Anders Latif <anders@github.com>
2018-10-10 13:47:39 +0200

    can now send form to server

--- 7._Upload/1._Video_Site/public/upload/upload.js
+++ 7._Upload/1._Video_Site/public/upload/upload.js
@@ -1,6 +1,17 @@
+let fileValid = false;
 
+function submittingForm() {
+    return fileValid;
+}
 
 function fileAdded(files) {
-    console.log("files? ", );
-    // 536870912
+    validateFile(files[0]);
+}
+
+function validateFile(file) {
+    if (file.type === "video/mp4" || file.type === "video/ogg" && file.size <= 536870912) {
+        fileValid = true;
+    } else {
+        fileValid = false;
+    }
 }
