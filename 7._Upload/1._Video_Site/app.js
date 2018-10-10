108be6ec586404885789b5a6e244f93911ff743a
Anders Latif <anders@github.com>
2018-10-10 14:25:56 +0200

    can't upload file

--- 7._Upload/1._Video_Site/app.js
+++ 7._Upload/1._Video_Site/app.js
@@ -5,6 +5,13 @@ const videos = ["Car-driving.mp4", "Raindrops.mp4"];
 
 app.use(express.static('public'));
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const fileUpload = require('express-fileupload');
+app.use(fileUpload());
+
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/public/home/home.html");
 });
@@ -13,19 +20,22 @@ app.get("/featured-videos", function(req, res) {
     res.send(videos);
 });
 
-// upload: remember to give id on server-side
-// uniqid
 
-// todo get video id from query string
+
 app.get("/player", function(req, res) {
     res.sendFile(__dirname + "/public/player/player.html");
 });
 
 app.get("/upload", function(req, res) {
+    console.log("file: ", req.files);
     res.sendFile(__dirname + "/public/upload/upload.html");
 });
 
 app.post("/upload", function(req, res) {
+    
+    
+    // upload: remember to give id on server-side
+    // uniqid
     res.send("File uploaded correctly!");
 });
 
--- 7._Upload/1._Video_Site/public/upload/upload.js
+++ 7._Upload/1._Video_Site/public/upload/upload.js
@@ -6,12 +6,31 @@ function submittingForm() {
 
 function fileAdded(files) {
     validateFile(files[0]);
+    if (fileValid) {
+        swal({
+            type: 'success',
+            timer: 10000,
+            title: 'Yaaaaay!',
+            text: 'File uploaded correctly',
+          })
+    } else {
+        const fileTypeValid = files[0].type === "video/mp4" || files[0].type === "video/ogg";
+        swal({
+            type: 'error',
+            timer: 10000,
+            title: 'Oops...',
+            text: 'Something went wrong!',
+            footer: fileTypeValid ? "Size is too big. Videos under 512MB only." : "File type not valid. mp4 or ogg only."
+          })
+    }
 }
 
 function validateFile(file) {
-    if (file.type === "video/mp4" || file.type === "video/ogg" && file.size <= 536870912) {
+    const fileTypeValid = file.type === "video/mp4" || file.type === "video/ogg";
+    if (file.size <= 536870912 && fileTypeValid) {
         fileValid = true;
     } else {
         fileValid = false;
     }
+    console.log(fileValid);
 }
