594e440723639d91366442a882de796f3614f492
Anders Latif <anders@github.com>
2018-10-24 13:43:32 +0200

    created my first insert / select

--- 9._Final_Video_Site/3.Knex_Migration/app.js
+++ 9._Final_Video_Site/3.Knex_Migration/app.js
@@ -1,1 +1,24 @@
-console.log("inside of app.js")
+// require the libraries
+const Knex = require("knex");
+const Model = require("objection").Model;
+const knexConfig = require("./knexfile");
+
+// use the driver and connect locally to my mysql
+const knex = Knex(knexConfig.development);
+
+// connect knex with objection and put query methods on the models
+Model.knex(knex);
+
+// convenience object.. easy access to the models
+const db = {
+    "knex": knex,
+    "Message": require("./models/Message")
+};
+
+// db.Message.query().insert({"message": "Hi, how are you?"}).then(persistedData => {
+//     console.log(persistedData);
+// });
+
+db.Message.query().select().then(foundData => {
+    console.log(foundData);
+})
