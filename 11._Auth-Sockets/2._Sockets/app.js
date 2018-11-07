8b2b1e94023cd2ce8a48ec11a4296295f48c3656
Anders Latif <anders@github.com>
2018-11-07 13:43:52 +0100

    finished socket.io example

--- 11._Auth-Sockets/2._Sockets/app.js
+++ 11._Auth-Sockets/2._Sockets/app.js
@@ -4,7 +4,22 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
 
+io.on('connect', socket => {
+    socket.on('send-color', data => {
+        // emits to all the sockets
+        io.emit("here's the color", data);
+
+        // emits only to the specific socket
+        //socket.emit("here's the color", data);
+        
+        // emits to all but the socket itself
+        //socket.broadcast.emit("here's the color", data);
+    })
+})
 
 server.listen(3000, (err) => {
     if (err) throw err;
